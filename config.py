e39d5c8d517d94d34a6b401b59de12083855c5a5
miocjlutejllo <miocjlutejllo@mail.ru>
2021-09-29 17:52:44 +0500

    first

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ DBHOST =  str(os.getenv("DBHOST"))
 DATABASE = str(os.getenv("DATABASE"))
 
 #POSTGRES_URI = f"postgresql://{PGUSER}:{PGPASSWORD}@{DBHOST}/{DATABASE}"
-POSTGRES_URI = "postgres://brayggjzcoaaaw:85cd98d2e45702185f02e46ada31f43243d1563e0d6176ec26f456cc93911f07@ec2-54-74-156-137.eu-west-1.compute.amazonaws.com:5432/d5srkl8m3br4og"
+POSTGRES_URI = "postgres://dfaresgsvnykxo:5e217af37e9b8e773e36c6d0dd9ea25d6b5b2f07b614542aa53f961495d74c20@ec2-54-74-77-126.eu-west-1.compute.amazonaws.com:5432/d2mnu5gqc31t2m"
